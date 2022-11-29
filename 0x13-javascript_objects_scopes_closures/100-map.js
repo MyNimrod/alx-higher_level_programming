55892c3a97b054250320a5d430b5f374a5681221
MyNimrod <behailugobeziefentaw@gmail.com>
2022-11-29 12:08:01 -0800

    11. Factor index changing code

--- 0x13-javascript_objects_scopes_closures/100-map.js
+++ 0x13-javascript_objects_scopes_closures/100-map.js
@@ -1,5 +1,6 @@
 #!/usr/bin/node
 // maps an array, multiplying each element by its index
-const list = require('./100-data.js').list;
+const list = require('./100-data').list;
+const mapped = list.map(function (value, index) { return value * index; });
 console.log(list);
-console.log(list.map((element, index) => element * index));
+console.log(mapped);
